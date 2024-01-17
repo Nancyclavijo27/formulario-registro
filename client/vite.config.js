a7ec2bccb693d1e5cf87417610715c9a8cefac7b
Nancy Clavijo Varela <99740842+Nancyclavijo27@users.noreply.github.com>
2024-01-17 15:53:27 -0500

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Ajusta según la ubicación de tu proyecto
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ajusta según tu preferencia
+  },
+});
